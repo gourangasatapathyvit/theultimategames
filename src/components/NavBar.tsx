8664a96e532cfc660a0ecc5912c54d660989740e
Gouranga Satapathy <pulusatapathy@gmail.com>
2023-04-22 15:12:38 +0530

    implemented dark mode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,32 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image, Text, Button, useColorMode } from "@chakra-ui/react";
+import { MdDarkMode, MdLightMode } from "react-icons/md";
+
 import logo from "../assets/logo.webp";
 
 const NavBar = () => {
+  const { colorMode, setColorMode } = useColorMode();
+
+  const changeColorMode = () => {
+    {
+      colorMode === "light" ? setColorMode("dark") : setColorMode("light");
+    }
+  };
   return (
     <>
-      <HStack>
-        <Image src={logo} boxSize={"60px"} />
-        <Text textTransform={"uppercase"}>{"lorem"}</Text>
+      <HStack justifyContent="space-between" p={3}>
+        <HStack>
+          <Image src={logo} boxSize={"60px"} />
+          <Text textTransform={"uppercase"}>{"lorem"}</Text>
+        </HStack>
+
+        <HStack>
+          <Button colorScheme="green" onClick={changeColorMode}>
+            <>
+              {colorMode === "light" && <MdLightMode />}
+              {colorMode === "dark" && <MdDarkMode />}
+            </>
+          </Button>
+        </HStack>
       </HStack>
     </>
   );
